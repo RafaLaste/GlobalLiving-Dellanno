3273002a943b89e0c1367cd6e891a5ec72a3cba3
Rafael Laste <rafinhalaste@hotmail.com>
2025-04-11 10:07:11 -0300

    Correção vídeo

--- src/Components/ShowroomVideo.tsx
+++ src/Components/ShowroomVideo.tsx
@@ -24,6 +24,7 @@ const ShowroomVideo: React.FC = () => {
                     ref={videoRef}
                     className="w-full h-auto"
                     src="/content/videos/video-showroom.webm"
+                    poster="/content/display/6a0c178d672f54a8ac36e3a73b6b503f.jpg"
                 />
 
                 <button
